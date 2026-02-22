029040f8f35647403ee5259a6421b78eb77666ec
Julien Wintz <julien.wintz@inria.fr>
2026-02-22 11:14:56 +0100

    fix(shared): move GeometrySizeTracker to HyaloShared

--- Sources/Hyalo/Core/HyaloShared.swift
+++ Sources/Hyalo/Core/HyaloShared.swift
@@ -76,41 +76,7 @@ enum HyaloDesign {
 
 // MARK: - Geometry Tracking
 
-/// Tracks geometry changes and reports them via a callback
-struct GeometrySizeTracker: View {
-    let onChange: (CGFloat) -> Void
-    let dimension: Dimension
-    let onUpdate: (() -> Void)?
-
-    enum Dimension {
-        case width
-        case height
-    }
-
-    init(dimension: Dimension = .width, onChange: @escaping (CGFloat) -> Void, onUpdate: (() -> Void)? = nil) {
-        self.dimension = dimension
-        self.onChange = onChange
-        self.onUpdate = onUpdate
-    }
-
-    init(dimension: Dimension = .width, onChange: @escaping (CGFloat) -> Void) {
-        self.init(dimension: dimension, onChange: onChange, onUpdate: nil)
-    }
-
-    var body: some View {
-        GeometryReader { geometry in
-            Color.clear
-                .onAppear {
-                    let size = dimension == .width ? geometry.size.width : geometry.size.height
-                    onChange(size)
-                }
-                .onChange(of: dimension == .width ? geometry.size.width : geometry.size.height) { _, newSize in
-                    onChange(newSize)
-                    onUpdate?()
-                }
-        }
-    }
-}
+ 
 
 // MARK: - Vibrancy Background
 
