c670e0c9e9fee90d068aa8f5cf7af4e503663f60
Julien Wintz <julien.wintz@inria.fr>
2026-03-03 23:03:42 +0100

    feat(windows): change shackle alignment to below

--- init/init-windows.el
+++ init/init-windows.el
@@ -12,7 +12,7 @@
   :demand t
   :config
   (setq shackle-default-rule '(:select t)
-        shackle-default-alignment 'bottom
+        shackle-default-alignment 'below
         shackle-default-size 0.4
         shackle-select-reused-windows t)
 
@@ -22,41 +22,41 @@
           ("\\*Async Shell.*\\*" :regexp t :ignore t)
 
           ;; Help & documentation - bottom popup, select on open
-          (help-mode             :align t :size 0.42 :select t)
-          (helpful-mode          :align t :size 0.42 :select t)
-          (apropos-mode          :align t :size 0.42 :select t)
-          ("*Help*"              :align t :size 0.42 :select t)
-          ("*Apropos*"           :align t :size 0.42 :select t)
-          ("\\*Man .*"           :regexp t :align t :size 0.5)
-          ("\\*WoMan .*"         :regexp t :align t :size 0.5)
+          (help-mode             :align 'below :size 0.42 :select t)
+          (helpful-mode          :align 'below :size 0.42 :select t)
+          (apropos-mode          :align 'below :size 0.42 :select t)
+          ("*Help*"              :align 'below :size 0.42 :select t)
+          ("*Apropos*"           :align 'below :size 0.42 :select t)
+          ("\\*Man .*"           :regexp t :align 'below :size 0.5)
+          ("\\*WoMan .*"         :regexp t :align 'below :size 0.5)
 
           ;; Info manual
-          ("*info*"              :align t :size 0.45 :select t)
+          ("*info*"              :align 'below :size 0.45 :select t)
 
           ;; Compilation - bottom popup, don't steal focus
-          (compilation-mode      :align t :size 0.3 :noselect t)
-          ("*compilation*"       :align t :size 0.3 :noselect t)
-          ("*Shell Command*"     :align t :size 0.2 :noselect t)
-          ("*grep*"              :align t :size 0.3 :noselect t)
-          ("*Occur*"             :align t :size 0.3 :noselect t)
+          (compilation-mode      :align 'below :size 0.3 :noselect t)
+          ("*compilation*"       :align 'below :size 0.3 :noselect t)
+          ("*Shell Command*"     :align 'below :size 0.2 :noselect t)
+          ("*grep*"              :align 'below :size 0.3 :noselect t)
+          ("*Occur*"             :align 'below :size 0.3 :noselect t)
 
           ;; Completions - small popup, don't select
-          ("*Completions*"       :align t :size 0.2 :noselect t)
+          ("*Completions*"       :align 'below :size 0.2 :noselect t)
 
           ;; Version control
-          ("*vc-diff*"           :align t :size 0.4)
-          ("*vc-change-log*"     :align t :size 0.3)
+          ("*vc-diff*"           :align 'below :size 0.4)
+          ("*vc-change-log*"     :align 'below :size 0.3)
 
           ;; Transient/temporary buffers - don't select
-          ("*Messages*"          :align t :size 0.3 :noselect t)
-          ("*Warnings*"          :align t :size 0.25 :noselect t)
-          ("*Backtrace*"         :align t :size 0.4 :noselect t)
+          ("*Messages*"          :align 'below :size 0.3 :noselect t)
+          ("*Warnings*"          :align 'below :size 0.25 :noselect t)
+          ("*Backtrace*"         :align 'below :size 0.4 :noselect t)
 
           ;; Flymake/Flycheck diagnostics
-          ("\\*Flymake diagnostics.*" :regexp t :align t :size 0.3 :noselect t)
+          ("\\*Flymake diagnostics.*" :regexp t :align 'below :size 0.3 :noselect t)
 
           ;; Eldoc (when displayed in separate buffer)
-          ("*eldoc*"             :align t :size 0.2 :noselect t)
+          ("*eldoc*"             :align 'below :size 0.2 :noselect t)
 
           ;; Magit - display in same window
           (magit-status-mode     :select t :same t)
@@ -64,10 +64,16 @@
           (magit-diff-mode       :select t :same t)
 
           ;; Hyalo build - bottom popup
-          ("*hyalo-build*"       :align t :size 0.3 :select t)
+          ("*hyalo-build*"       :align 'below :size 0.3 :select t)
 
           ;; Process lists
-          ("*Process List*"      :align t :size 0.3 :noselect t)
+          ("*Process List*"      :align 'below :size 0.3 :noselect t)
+
+          ;; Buffer list - bottom popup, select on open
+          (Buffer-menu-mode      :align 'below :size 0.4 :select t)
+          ("*Buffer List*"       :align 'below :size 0.4 :select t)
+          (ibuffer-mode          :align 'below :size 0.4 :select t)
+          ("*Ibuffer*"           :align 'below :size 0.4 :select t)
           ))
 
   (shackle-mode 1))
